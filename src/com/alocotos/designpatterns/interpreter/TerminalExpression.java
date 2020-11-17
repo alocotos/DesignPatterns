d8427b22264e94f13c26577ea0a7a8a8f6275173
Andrew Locotos <alocotos@gmail.com>
2020-11-17 00:55:21 -0500

    Added interpreter pattern example.

---
+++ src/com/alocotos/designpatterns/interpreter/TerminalExpression.java
@@ -0,0 +1,28 @@
+package com.alocotos.designpatterns.interpreter;
+
+import java.util.StringTokenizer;
+
+public class TerminalExpression implements Expression {
+
+	private String data;
+	
+	public TerminalExpression(String data) {
+		this.data = data;
+	}
+	
+	@Override
+	public boolean interpret(String str) {
+		
+		StringTokenizer st = new StringTokenizer(str);
+		
+		while(st.hasMoreTokens()) {
+			String test = st.nextToken();
+			if(test.contentEquals(data)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}
